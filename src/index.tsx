e951f97ab481fb47a213b6f11d45ead76cd9f7ac
Leandro da silva <leandrosilva.hair@gmail.com>
2022-10-24 22:17:51 -0300

    router in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import "normalize.css";
 import './index.css';
-import Inicio from './pages/Inicio/index';
-
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
